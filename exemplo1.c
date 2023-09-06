a38ce7a8c07a9f4bacb70cffeb260852fc91c66e
Rodrigo Fernandes <rodmironiuc@gmail.com>
2023-09-06 16:54:52 -0300

    exercicios

---
+++ exemplo1.c
@@ -0,0 +1,12 @@
+// Criacao de Processos
+
+#include <stdio.h>
+#include <unistd.h>
+int main(void) {
+    int x;
+    x = 0;
+    fork();
+    x = 1;
+    printf ("I am process %ld and my x is %d\n", (long)getpid(), x);
+    return 0;
+}
